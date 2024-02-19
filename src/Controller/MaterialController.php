552872323c909e6223d754c296bcbd9b7bcfb6d6
Perroquot <trystan.colin80@gmail.com>
2024-02-19 11:50:35 +0100

    fix all fixtures

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MaterialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class MaterialController extends AbstractController
 {
     #[Route('/material', name: 'app_material')]
-    public function index(): Response
+    public function index(MaterialRepository $materialRepository): Response
     {
         return $this->render('material/index.html.twig', [
-            'materials' => 'MaterialController',
+            'materials' => $materialRepository->findAll(),
         ]);
     }
 }
