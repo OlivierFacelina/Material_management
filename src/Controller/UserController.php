5a9a2faa3fd807e041f9319e8dc56b6f1aa12ea1
OlivierF <olivier.facelina@gmail.com>
2024-02-19 08:49:05 +0100

    Création du controller user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/user', name: 'user_')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
